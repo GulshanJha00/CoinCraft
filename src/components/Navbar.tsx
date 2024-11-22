52032fef5edecb8e7734e52e402c73e20f7fdef3
GulshanJha00 <gk4763549@gmail.com>
2024-11-22 14:28:03 +0530

    changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,26 @@
 "use client";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 const Navbar: React.FC = () => {
   const { setTheme, theme } = useTheme(); // Theme context for toggling light/dark modes
+  const [mounted, setMounted] = useState(false); // State to track if the component has mounted
 
- 
+  // Use useEffect to set mounted to true after component mounts
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Return null during SSR to avoid mismatch in HTML
+  if (!mounted) return null;
 
   return (
     <>
       {/* Navbar Container */}
       <div className="bg-blue-900 flex justify-between items-center w-full h-20 px-8 py-4 shadow-lg">
         
-        {/* Title Section on the left*/}
+        {/* Title Section on the left */}
         <div className="text-yellow-400 font-bold lg:text-3xl sm:text-xl text-md">
           <h1>Coin Verse</h1>
         </div>
@@ -27,15 +33,13 @@ const Navbar: React.FC = () => {
             
           </div>
 
-          {/* Theme Toggle Button using shadcn */}
+          {/* Theme Toggle Button */}
           <div>
             <button
-              
-              
               className="z-50 bg-transparent text-white h-[3rem] w-[3rem] border-2 border-white rounded-full flex justify-center items-center hover:bg-gray-700 focus:ring-2 focus:ring-blue-500 transition-all"
               onClick={() => setTheme(theme === "dark" ? "light" : "dark")} // Toggle theme
             >
-              {/* Conditionally render icon based on current theme chosen above*/}
+              {/* Conditionally render icon based on current theme chosen above */}
               {theme === "dark" ? (
                 <Sun className="h-[1.5rem] w-[1.5rem] transition-all" />
               ) : (
